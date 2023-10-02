bcd63e6852df917e3dc6bbef305a45d5f01a3879
Kyaw Zaw Lwin <75203193+KyawZawLwin6394@users.noreply.github.com>
2023-10-02 10:31:24 +0630

    here

--- app/controllers/damagedItemController.js
+++ app/controllers/damagedItemController.js
@@ -1,7 +1,9 @@
 'use strict';
 const DamagedItem = require('../models/damagedItem');
 const Stock = require('../models/stock');
-
+const ProcedureItems = require('../models/procedureItem');
+const AccessoryItems = require('../models/accessoryItem');
+const MedicineItems = require('../models/medicineItem');
 exports.listAllDamagedItems = async (req, res) => {
   try {
     let result = await DamagedItem.find({ isDeleted: false }).populate({
@@ -32,9 +34,38 @@ exports.getDamagedItem = async (req, res) => {
 
 exports.createDamagedItem = async (req, res, next) => {
   try {
-    const { relatedStockRecord, damagedCurrentQty, purchasePrice } = req.body;
-
+    const { relatedStockRecord, damagedCurrentQty, purchasePrice, relatedProcedureItems, relatedMedicineItems, relatedAccessoryItems } = req.body;
     const stockUpdate = await Stock.findOneAndUpdate({ _id: relatedStockRecord }, { $inc: { qty: -damagedCurrentQty, batchPrice: -(damagedCurrentQty * purchasePrice) } }, { new: true })
+
+    // Handle relatedProcedureItems
+    if (relatedProcedureItems) {
+      const stock = await Stock.find({ relatedProcedureItems: relatedProcedureItems })
+      if (stock.length === 0) return res.status(200).send({ error: true, message: 'ProcedureItems Stock Not Found!' })
+      const totalQty = stock.reduce((accumulator, current) => accumulator + current.qty, 0)
+      console.log(totalQty)
+      const update = await ProcedureItems.findOneAndUpdate({ _id: relatedProcedureItems }, { currentQuantity: totalQty })
+    }
+
+    // Handle relatedMedicineItems
+    if (relatedMedicineItems) {
+      const stock = await Stock.find({ relatedMedicineItems: relatedMedicineItems })
+      if (stock.length === 0) return res.status(200).send({ error: true, message: 'MedicineItems Stock Not Found!' })
+      const totalQty = stock.reduce((accumulator, current) => accumulator + current.qty, 0)
+      console.log(totalQty)
+      // Update MedicineItems
+      const update = await MedicineItems.findOneAndUpdate({ _id: relatedMedicineItems }, { currentQuantity: totalQty })
+    }
+
+    // Handle relatedAccessoryItems
+    if (relatedAccessoryItems) {
+      const stock = await Stock.find({ relatedAccessoryItems: relatedAccessoryItems })
+      if (stock.length === 0) return res.status(200).send({ error: true, message: 'AccessoryItems Stock Not Found!' })
+      const totalQty = stock.reduce((accumulator, current) => accumulator + current.qty, 0)
+      console.log(totalQty)
+      // Update AccessoryItems
+      const update = await AccessoryItems.findOneAndUpdate({ _id: relatedAccessoryItems }, { currentQuantity: totalQty })
+    }
+
     const newDamagedItem = new DamagedItem(req.body);
     const result = await newDamagedItem.save();
     res.status(200).send({
@@ -47,6 +78,7 @@ exports.createDamagedItem = async (req, res, next) => {
     return res.status(500).send({ "error": true, message: error.message })
   }
 };
+
 
 exports.updateDamagedItem = async (req, res, next) => {
   try {
